3e447b4eb89bffe9d48b55523017bbe50cd57510
rashel9255 <ibrahim2000167@gmail.com>
2026-02-03 19:54:22 +0600

    delete unnecessary files

--- src/app/services/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const ServiceDetails = async ({ params }: { params: Promise<{ id: string }> }) => {
-    const { id } = await params;
-    return <div>service details page of {id}</div>;
-};
-
-export default ServiceDetails;
